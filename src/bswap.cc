a3d898cdc26c4a27d4a8ac5d6d36fdb0918fc748
Zach Bjornson <bjornson@stanford.edu>
2016-06-04 16:55:10 -0700

    support more compilers (clang, linux generic, other)

--- src/bswap.cc
+++ src/bswap.cc
@@ -1,7 +1,35 @@
 #include <nan.h>
 
-#if defined(_MSC_VER)
+#if defined(__GNUC__) || defined(__clang__)
+#define BSWAP_INTRINSIC_2(x) __builtin_bswap16(x)
+#define BSWAP_INTRINSIC_4(x) __builtin_bswap32(x)
+#define BSWAP_INTRINSIC_8(x) __builtin_bswap64(x)
+#elif defined(__linux__)
+#include <byteswap.h>
+#define BSWAP_INTRINSIC_2(x) bswap_16(x)
+#define BSWAP_INTRINSIC_4(x) bswap_32(x)
+#define BSWAP_INTRINSIC_8(x) bswap_64(x)
+#elif defined(_MSC_VER)
 #include <intrin.h>
+#define BSWAP_INTRINSIC_2(x) _byteswap_ushort(x);
+#define BSWAP_INTRINSIC_4(x) _byteswap_ulong(x);
+#define BSWAP_INTRINSIC_8(x) _byteswap_uint64(x);
+#else
+#define BSWAP_INTRINSIC_2(x) (x << 8) | (x >> 8)
+#define BSWAP_INTRINSIC_4(x)                                                  \
+  ((x & 0xFF) << 24) |                                                        \
+  ((x & 0xFF00) << 8) |                                                       \
+  ((x >> 8) & 0xFF00) |                                                       \
+  ((x >> 24) & 0xFF)
+#define BSWAP_INTRINSIC_8(x)                                                  \
+  ((x & 0xFF00000000000000ull) >> 56) |                                       \
+  ((x & 0x00FF000000000000ull) >> 40) |                                       \
+  ((x & 0x0000FF0000000000ull) >> 24) |                                       \
+  ((x & 0x000000FF00000000ull) >> 8)  |                                       \
+  ((x & 0x00000000FF000000ull) << 8)  |                                       \
+  ((x & 0x0000000000FF0000ull) << 24) |                                       \
+  ((x & 0x000000000000FF00ull) << 40) |                                       \
+  ((x & 0x00000000000000FFull) << 56)
 #endif
 
 using namespace v8;
@@ -9,26 +37,16 @@ using namespace v8;
 void flip16(Local<TypedArray> in) {
   Nan::TypedArrayContents<uint16_t> events(in);
   size_t eventsLength = in->Length();
-
   for (size_t i = 0; i < eventsLength; i++) {
-#if defined(_MSC_VER)
-    (*events)[i] = _byteswap_ushort((*events)[i]);
-#elif defined(__GNUC__)
-    (*events)[i] = __builtin_bswap16((*events)[i]);
-#endif
+    (*events)[i] = BSWAP_INTRINSIC_2((*events)[i]);
   }
 }
 
 void flip32(Local<TypedArray> in) {
   Nan::TypedArrayContents<uint32_t> events(in);
   size_t eventsLength = in->Length();
-
   for (size_t i = 0; i < eventsLength; i++) {
-#if defined(_MSC_VER)
-    (*events)[i] = _byteswap_ulong((*events)[i]);
-#elif defined(__GNUC__)
-    (*events)[i] = __builtin_bswap32((*events)[i]);
-#endif
+    (*events)[i] = BSWAP_INTRINSIC_4((*events)[i]);
   }
 }
 
@@ -37,11 +55,7 @@ void flip64(Local<TypedArray> in) {
   size_t eventsLength = in->Length();
 
   for (size_t i = 0; i < eventsLength; i++) {
-#if defined(_MSC_VER)
-    (*events)[i] = _byteswap_uint64((*events)[i]);
-#elif defined(__GNUC__)
-    (*events)[i] = __builtin_bswap64((*events)[i]);
-#endif
+    (*events)[i] = BSWAP_INTRINSIC_8((*events)[i]);
   }
 }
 
